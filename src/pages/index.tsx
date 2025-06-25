5a06d4ee796e47e9ec74bd70424297408ec6c098
John Li (Tet) <tet@tet.moe>
2025-06-25 02:26:22 -0700

    remove check

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
       requestAnimationFrame(drawCallback);
     };
     drawCallback();
-
+    
     return () => {
       rerun = false;
     };
@@ -40,98 +40,15 @@ export default function Home() {
     const interval = setInterval(() => {
       setCount((prev) => prev + 1);
     }, 100);
-    var nVer = navigator.appVersion;
-    var nAgt = navigator.userAgent;
-    var browserName = navigator.appName;
-    var fullVersion = "" + parseFloat(navigator.appVersion);
-    var majorVersion = parseInt(navigator.appVersion, 10);
-    var nameOffset, verOffset, ix;
-
-    // In Opera, the true version is after "OPR" or after "Version"
-    if ((verOffset = nAgt.indexOf("OPR")) != -1) {
-      browserName = "Opera";
-      fullVersion = nAgt.substring(verOffset + 4);
-      if ((verOffset = nAgt.indexOf("Version")) != -1)
-        fullVersion = nAgt.substring(verOffset + 8);
-    }
-    // In MS Edge, the true version is after "Edg" in userAgent
-    else if ((verOffset = nAgt.indexOf("Edg")) != -1) {
-      browserName = "Microsoft Edge";
-      fullVersion = nAgt.substring(verOffset + 4);
-    }
-    // In MSIE, the true version is after "MSIE" in userAgent
-    else if ((verOffset = nAgt.indexOf("MSIE")) != -1) {
-      browserName = "Microsoft Internet Explorer";
-      fullVersion = nAgt.substring(verOffset + 5);
-    }
-    // In Chrome, the true version is after "Chrome"
-    else if ((verOffset = nAgt.indexOf("Chrome")) != -1) {
-      browserName = "Chrome";
-      fullVersion = nAgt.substring(verOffset + 7);
-    }
-    // In Safari, the true version is after "Safari" or after "Version"
-    else if ((verOffset = nAgt.indexOf("Safari")) != -1) {
-      browserName = "Safari";
-      fullVersion = nAgt.substring(verOffset + 7);
-      if ((verOffset = nAgt.indexOf("Version")) != -1)
-        fullVersion = nAgt.substring(verOffset + 8);
-    }
-    // In Firefox, the true version is after "Firefox"
-    else if ((verOffset = nAgt.indexOf("Firefox")) != -1) {
-      browserName = "Firefox";
-      fullVersion = nAgt.substring(verOffset + 8);
-    }
-    // In most other browsers, "name/version" is at the end of userAgent
-    else if (
-      (nameOffset = nAgt.lastIndexOf(" ") + 1) <
-      (verOffset = nAgt.lastIndexOf("/"))
-    ) {
-      browserName = nAgt.substring(nameOffset, verOffset);
-      fullVersion = nAgt.substring(verOffset + 1);
-      if (browserName.toLowerCase() == browserName.toUpperCase()) {
-        browserName = navigator.appName;
-      }
-    }
-    // trim the fullVersion string at semicolon/space if present
-    if ((ix = fullVersion.indexOf(";")) != -1)
-      fullVersion = fullVersion.substring(0, ix);
-    if ((ix = fullVersion.indexOf(" ")) != -1)
-      fullVersion = fullVersion.substring(0, ix);
-
-    majorVersion = parseInt("" + fullVersion, 10);
-    if (isNaN(majorVersion)) {
-      fullVersion = "" + parseFloat(navigator.appVersion);
-      majorVersion = parseInt(navigator.appVersion, 10);
-    }
-
-    document.write(
-      "" +
-        "Browser name  = " +
-        browserName +
-        "<br>" +
-        "Full version  = " +
-        fullVersion +
-        "<br>" +
-        "Major version = " +
-        majorVersion +
-        "<br>" +
-        "navigator.appName = " +
-        navigator.appName +
-        "<br>" +
-        "navigator.userAgent = " +
-        navigator.userAgent +
-        "<br>"
-    );
+    
     return () => clearInterval(interval);
   }, []);
   return (
-    <>
-      <Head>
-        <title> Canvas Video Rendering Test {count}</title>
-      </Head>
-      <div
-        className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-      >
+    <><Head>
+      <title> Canvas Video Rendering Test {count}</title>
+    </Head><div
+      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    >
         <h1 className="text-4xl sm:text-6xl font-bold text-center">
           Canvas Video Rendering Test!
         </h1>
@@ -160,7 +77,6 @@ export default function Home() {
             ref={canvasRef}
           ></canvas>
         </main>
-      </div>
-    </>
+      </div></>
   );
 }
