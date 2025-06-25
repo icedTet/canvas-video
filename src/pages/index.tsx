bfd15d7f0bafe8e217957f519e68091b437abb34
John Li (Tet) <tet@tet.moe>
2025-06-25 01:27:12 -0700

    push count test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import React, { useEffect } from "react";
+import Head from "next/head";
+import React, { useEffect, useState } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,43 +30,52 @@ export default function Home() {
       requestAnimationFrame(drawCallback);
     };
     drawCallback();
-
+    
     return () => {
       rerun = false;
     };
   }, []);
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prev) => prev + 1);
+    }, 100);
+    return () => clearInterval(interval);
+  }, []);
   return (
-    <div
+    <><Head>
+      <title> Canvas Video Rendering Test {count}</title>
+    </Head><div
       className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <h1 className="text-4xl sm:text-6xl font-bold text-center">
-        Canvas Video Rendering Test!
-      </h1>
-      <main className="flex flex-col items-center justify-center gap-8">
-        <div className="w-full max-w-2xl">
-          <video
-            className="w-full rounded-lg shadow-lg"
-            loop
-            controls
-            content="true"
-            playsInline
-            ref={videoRef}
-          >
-            <source src="/lyd.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-        <p className="text-center text-lg sm:text-xl">
-          This is a test of rendering a video using the Canvas API in Next.js.
-        </p>
-        <canvas
-          id="canvas"
-          className="w-full max-w-2xl rounded-lg shadow-lg"
-          width="1280"
-          height="720"
-          ref={canvasRef}
-        ></canvas>
-      </main>
-    </div>
+        <h1 className="text-4xl sm:text-6xl font-bold text-center">
+          Canvas Video Rendering Test!
+        </h1>
+        <main className="flex flex-col items-center justify-center gap-8">
+          <div className="w-full max-w-2xl">
+            <video
+              className="w-full rounded-lg shadow-lg"
+              loop
+              controls
+              content="true"
+              playsInline
+              ref={videoRef}
+            >
+              <source src="/lyd.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+          <p className="text-center text-lg sm:text-xl">
+            This is a test of rendering a video using the Canvas API in Next.js.
+          </p>
+          <canvas
+            id="canvas"
+            className="w-full max-w-2xl rounded-lg shadow-lg"
+            width="1280"
+            height="720"
+            ref={canvasRef}
+          ></canvas>
+        </main>
+      </div></>
   );
 }
