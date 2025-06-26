a7fc9886b8bc6dc62a0149744d589de58a6a259d
John Li (Tet) <tet@tet.moe>
2025-06-25 21:14:12 -0700

    did not update the file url, causing all this havoc

--- src/pages/demux.tsx
+++ src/pages/demux.tsx
@@ -73,7 +73,7 @@ export const DemuxRender = () => {
     canvas.width = divRef.current?.clientWidth || 800; // Default width if divRef is not set
     canvas.height = divRef.current?.clientHeight || 600; // Default height if divRef is not setz
     (async () => {
-      const fileURL = "lyd.mkv"; // Replace with your file URL
+      const fileURL = "lyd.mp4"; // Replace with your file URL
       const headerInfos = await fetch(fileURL, {
         method: "HEAD",
         mode: "cors",
